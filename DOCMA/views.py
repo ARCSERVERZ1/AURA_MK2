2b2ac9c56cfb36b8a31e99423495859789fa18bb
Sanjay-Offc Lap <sanjaybaswa@titan.co.in>
2024-11-09 23:38:02 +0530

    50% edit working in docma

--- DOCMA/views.py
+++ DOCMA/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-import os
+import os , time
 from datetime import datetime
 from .models import *
 from django.http import JsonResponse
@@ -10,6 +10,7 @@ from rest_framework import permissions
 from django.contrib.auth.decorators import login_required
 import string, random
 import pyrebase
+import shutil
 
 firebaseConfig = {
     'apiKey': "AIzaSyAMoENs6AiTkSnAhuHuzwpGFkxeaAhGQB4",
@@ -24,6 +25,7 @@ firebaseConfig = {
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 bucket = firebase.storage()
+
 
 
 def encrypt_value(value):
@@ -194,7 +196,10 @@ def save_uploaded_file(file):
 
 
 def test(request):
-    return render(request, "test.html")
+    if os.path.exists('AURA_MK2/cloud'):
+        return JsonResponse({"Source": "Cloud"}, safe=False)
+    else:
+        return JsonResponse({"Source": "Local"}, safe=False)
 
 
 def add_doc_type(request, new_doc_type):
@@ -359,6 +364,21 @@ def home_menu_req():
     menu_but = home_menu.objects.all().order_by('app_priority')
     return menu_but
 
+### ------------ check here ------------ ###
+
+
+def give_path():
+    random_code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(9))
+    if os.path.exists('AURA_MK2/assets/cloud'):
+        upload_dir = 'AURA_MK2/assets/buffer/'+random_code+'/'
+        main_folder = 'Docmanger/'
+    else:
+        upload_dir = 'assets/buffer/'+random_code+'/'
+        main_folder = 'Test/'
+
+    if not os.path.exists(upload_dir): os.makedirs(upload_dir)
+    return upload_dir, main_folder
+
 
 def doc_manager_save_firebase(request):
     if request.method == 'POST':
@@ -371,18 +391,27 @@ def doc_manager_save_firebase(request):
         remarks = request.POST['remarks']
         value = request.POST['value']
 
-        upload_dir = 'assets/buffer/'
-        if not os.path.exists('Local'): upload_dir = 'AURA_MK2/' + upload_dir
-        if not os.path.exists(upload_dir): os.makedirs(upload_dir)
-        firebase_storage_path = 'DocManager/' + docType + '/' + refNum + '_' + docName + '/'
+
+
+        local_dir , firebase_folder = give_path()
+
+        # if os.path.exists('AURA_MK2/assets/cloud'):
+        #     upload_dir = 'AURA_MK2/assets/buffer/'+random_code+'/'
+        #     main_folder = 'Docmanger/'
+        # else:
+        #     upload_dir = 'assets/buffer/'+random_code+'/'
+        #     main_folder = 'Test/'
+
+
+        firebase_storage_path = firebase_folder + docType + '/' + refNum + '_' + docName + '/'
         files_data = ''
         for file in request.FILES.getlist('file'):
-            with open(upload_dir + str(file), 'wb+') as destination:
+            with open(local_dir +str(file), 'wb+') as destination:
                 for chunk in file.chunks():
                     destination.write(chunk)
-            bucket.child(firebase_storage_path + str(file)).put(upload_dir + str(file))
+            bucket.child(firebase_storage_path + str(file)).put(local_dir + str(file))
             files_data = files_data  + str(file) + '|'
-
+        shutil.rmtree(local_dir)
         enc_value = ''
         if docType == 'Bank_Cards':
             enc_value = encrypt_value(value)
@@ -401,8 +430,10 @@ def doc_manager_save_firebase(request):
                               remarks=remarks,
                               updated_by=request.user.username
                               )
-
         data.save()
+
+
+
     return render(request, "DocManager_form.html")
 
 
@@ -424,15 +455,78 @@ def doc_viewer_firebase(request, type):
             'valid': document.end_date,
             'file_path': path,}
 
-    context = {'data': viewer}
+    document_holders = doc_holder.objects.values_list('Holder', flat=True)
+    document_type = doc_type.objects.values_list('type', flat=True)
+    print(document_holders, "-----------------------------------------")
+
+
+    context = {
+        'document_holders': document_holders,
+        'document_type': document_type,
+        'data': viewer
+
+    }
 
     return render(request, "DocViewer.html", context)
 
-def migrate(request):
-    data = {}
-    for doc_folder in os.listdir('AURA_MK2/assets/Documents'):
-        data[doc_folder] = []
-        for doc in os.listdir('AURA_MK2/assets/Documents/'+doc_folder):
-            data[doc_folder].append(doc)
+def get_data_by_id_firebase(request, id):
+    q1 = docma_firebase.objects.filter(id=id)
 
-    return JsonResponse( data , safe = False)
+
+    data = {
+        'id': q1[0].id,
+        'name': q1[0].holder,
+        'type': q1[0].type,
+        'ref': q1[0].refnumber,
+        'sdate': q1[0].start_date,
+        'edate': q1[0].end_date,
+        'value': q1[0].value,
+        'remarks': q1[0].remarks
+    }
+
+    return JsonResponse(data, safe=False)
+
+def add_edit_document_firebase(requests):
+    edited_data = json.loads(requests.body)
+    print(edited_data , "Requested Edit data ")
+    data_id_instance = docma_firebase.objects.filter(id=edited_data['id'])
+
+    local_dir, firebase_folder = give_path()
+    enc_value = encrypt_value(edited_data['value']) if edited_data['type'] == 'Bank_Cards'  else edited_data['value']
+    for i in data_id_instance:
+        old_data = {
+        'ref_num' : i.refnumber,
+        'type' : i.type,
+        'holder' : i.holder,
+        'document_list' : i.document_list
+        }
+
+    # dwnld to buffer
+    cloud_des_path = f'{firebase_folder}{edited_data['type']}/{edited_data['ref'] + '_' + edited_data['name']}/'
+    print(local_dir)
+    for doc in old_data['document_list'].split('|'):
+        if doc != '':
+            cloud_source_path = f'{firebase_folder}{old_data['type']}/{old_data['ref_num'] + '_' + old_data['holder']}/{doc}'
+            # print(cloud_source_path , "----------")
+            print(cloud_source_path +'|'+ local_dir +'|'+doc , "------------")
+            # bucket.child('Test/Passport/156_Panisha/loading.gif').download('/', 'hi.gif')
+            bucket.child(cloud_source_path).download( '' , local_dir+ doc)
+            bucket.child(cloud_des_path + doc).put(local_dir + doc)
+
+    data_id_instance.update(
+        holder=edited_data['name'],
+        refnumber=edited_data['ref'],
+        document_path = cloud_des_path,
+        end_date=edited_data['edate'],
+        start_date=edited_data['sdate'],
+        value=enc_value,
+        type=edited_data['type'],
+        time_stamp=datetime.now(),
+        remarks=edited_data['remarks'],
+        updated_by=requests.user.username
+    )
+
+    shutil.rmtree(local_dir)
+
+    return JsonResponse({'status': 'True'}, safe=False)
+
